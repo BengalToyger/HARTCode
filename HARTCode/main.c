983d5f2e9bdd2f32d9d1a1e9065c2a9531d6eadd
BengalToyger <bengaltoyger@gmail.com>
2017-05-23 17:02:56 -0700

    Added unit test files

--- HARTCode/main.c
+++ HARTCode/main.c
@@ -11,7 +11,6 @@
 
 int main(void)
 {
-	InitGPS();
 	while(1){};
 }
 
---
+++ HARTCode/unittest.h
@@ -0,0 +1,24 @@
+#ifndef UNITTEST
+#define UNITTEST
+
+#include "RocketTiming.h"
+#include "MPU6000.h"
+#include "AT24CMO2.h"
+#include "USARTComm.h"
+#include "radio_ctl.h"
+#include "PAM7Q.h"
+#include "MS5607.h"
+#include "Firing.h"
+#include "Kalman.h"
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+
+
+
+
+#endif
